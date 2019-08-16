3397783f3fa319d209cfd601d3942aef6a006572
gitomehbranson <sirbranson67@gmail.com>
2019-08-16 11:55:33 +0300

    fix: test build

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -13,7 +13,7 @@ class Navbar extends React.Component {
             <img
               alt="Logo"
               style={{ width: 150 }}
-              src="./assets/media/logos/logoV1.png"
+              src="./assets/media/logos/logo-v2.png"
             />
           </a>
         </div>
@@ -356,6 +356,130 @@ class Navbar extends React.Component {
                   className="kt-menu__link kt-menu__toggle"
                 >
                   <span className="kt-menu__link-text">PickUp and DropOff</span>
+                  <i className="kt-menu__hor-arrow la la-angle-down" />
+                  <i className="kt-menu__ver-arrow la la-angle-right" />
+                </a>
+                <div className="kt-menu__submenu kt-menu__submenu--classic kt-menu__submenu--left">
+                  <ul className="kt-menu__subnav">
+                    <li
+                      className="kt-menu__item  kt-menu__item--submenu"
+                      data-ktmenu-submenu-toggle="hover"
+                      aria-haspopup="true"
+                    >
+                      <a
+                        href="javascript:;"
+                        className="kt-menu__link kt-menu__toggle"
+                      >
+                        <i className="kt-menu__link-icon flaticon-rocket" />
+                        <span className="kt-menu__link-text">Widgets</span>
+                        <i className="kt-menu__hor-arrow la la-angle-right" />
+                        <i className="kt-menu__ver-arrow la la-angle-right" />
+                      </a>
+                      <div className="kt-menu__submenu kt-menu__submenu--classic kt-menu__submenu--right">
+                        <ul className="kt-menu__subnav">
+                          <li className="kt-menu__item " aria-haspopup="true">
+                            <a
+                              href="demo5/components/widgets/charts.html"
+                              className="kt-menu__link "
+                            >
+                              <i className="kt-menu__link-bullet kt-menu__link-bullet--dot">
+                                <span />
+                              </i>
+                              <span className="kt-menu__link-text">Charts</span>
+                            </a>
+                          </li>
+                          <li className="kt-menu__item " aria-haspopup="true">
+                            <a
+                              href="demo5/components/widgets/general.html"
+                              className="kt-menu__link "
+                            >
+                              <i className="kt-menu__link-bullet kt-menu__link-bullet--dot">
+                                <span />
+                              </i>
+                              <span className="kt-menu__link-text">
+                                General
+                              </span>
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+
+              <li
+                className="kt-menu__item  kt-menu__item--submenu kt-menu__item--rel"
+                data-ktmenu-submenu-toggle="click"
+                aria-haspopup="true"
+              >
+                <a
+                  href="javascript:;"
+                  className="kt-menu__link kt-menu__toggle"
+                >
+                  <span className="kt-menu__link-text">Reports</span>
+                  <i className="kt-menu__hor-arrow la la-angle-down" />
+                  <i className="kt-menu__ver-arrow la la-angle-right" />
+                </a>
+                <div className="kt-menu__submenu kt-menu__submenu--classic kt-menu__submenu--left">
+                  <ul className="kt-menu__subnav">
+                    <li
+                      className="kt-menu__item  kt-menu__item--submenu"
+                      data-ktmenu-submenu-toggle="hover"
+                      aria-haspopup="true"
+                    >
+                      <a
+                        href="javascript:;"
+                        className="kt-menu__link kt-menu__toggle"
+                      >
+                        <i className="kt-menu__link-icon flaticon-rocket" />
+                        <span className="kt-menu__link-text">Widgets</span>
+                        <i className="kt-menu__hor-arrow la la-angle-right" />
+                        <i className="kt-menu__ver-arrow la la-angle-right" />
+                      </a>
+                      <div className="kt-menu__submenu kt-menu__submenu--classic kt-menu__submenu--right">
+                        <ul className="kt-menu__subnav">
+                          <li className="kt-menu__item " aria-haspopup="true">
+                            <a
+                              href="demo5/components/widgets/charts.html"
+                              className="kt-menu__link "
+                            >
+                              <i className="kt-menu__link-bullet kt-menu__link-bullet--dot">
+                                <span />
+                              </i>
+                              <span className="kt-menu__link-text">Charts</span>
+                            </a>
+                          </li>
+                          <li className="kt-menu__item " aria-haspopup="true">
+                            <a
+                              href="demo5/components/widgets/general.html"
+                              className="kt-menu__link "
+                            >
+                              <i className="kt-menu__link-bullet kt-menu__link-bullet--dot">
+                                <span />
+                              </i>
+                              <span className="kt-menu__link-text">
+                                General
+                              </span>
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+
+              <li
+                className="kt-menu__item  kt-menu__item--submenu kt-menu__item--rel"
+                data-ktmenu-submenu-toggle="click"
+                aria-haspopup="true"
+              >
+                <a
+                  href="javascript:;"
+                  className="kt-menu__link kt-menu__toggle"
+                >
+                  <span className="kt-menu__link-text">SMS & Email Communication</span>
                   <i className="kt-menu__hor-arrow la la-angle-down" />
                   <i className="kt-menu__ver-arrow la la-angle-right" />
                 </a>
