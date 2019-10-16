9a4767d7754de452d8b74b7490cd6c87a6fed691
BransonGitomeh <sirbranson@gmail.com>
2019-10-16 12:29:49 +0300

    messaging UI improvements

--- src/pages/communications/components/message.js
+++ src/pages/communications/components/message.js
@@ -1,25 +1,24 @@
 import React from 'react'
 
-const MessageView = ({ onClickSend, onClickSwitch, onChange }) => {
-    return (
+const MessageView = ({ type, onClickSend, onClickSwitch, onChange }) => {
+  return (
     <div className="kt-portlet">
       <div className="kt-portlet__head">
         <div class="kt-portlet__head-label">
-					<span class="kt-portlet__head-icon">
-						<i class="la la-comment-o"></i>
-					</span>
+          <span class="kt-portlet__head-icon">
+            <i class="la la-comment-o"></i>
+          </span>
           <h3 className="kt-portlet__head-title kt-heading kt-heading--center kt-heading--thin">
             Send Message
-					</h3>			
-				</div>
+					</h3>
+        </div>
         <div class="kt-portlet__head-toolbar">
-        <div class="kt-portlet__head-toolbar">
-					<div class="kt-portlet__head-group">
-						<button type="button" class="btn btn-sm btn-outline-brand btn-elevate btn-pill" onClick={() => onClickSwitch('sms')}>SMS</button>&nbsp;
-						<button type="button" class="btn btn-sm btn-outline-brand btn-elevate btn-pill" onClick={() => onClickSwitch('email')}>Email</button>
-					</div>
-				</div>
-				</div>
+          <div class="kt-portlet__head-toolbar">
+            <div class="kt-portlet__head-group">
+              {["sms", "email"].map(possibleType => <button type="button" style={{ "margin": "2px", color: possibleType === type ? "white" : "" }} class={`btn btn-sm btn-outline-brand ${possibleType === type ? "btn-success" : ""} btn-elevate btn-pill`} onClick={() => onClickSwitch(possibleType.toLowerCase())}>{possibleType.toUpperCase()}</button>)}
+            </div>
+          </div>
+        </div>
       </div>
       <div className="kt-portlet__body">
         <textarea className="form-control my-2" onChange={onChange}></textarea>
@@ -28,7 +27,7 @@ const MessageView = ({ onClickSend, onClickSwitch, onChange }) => {
         <button className="btn btn-outline-success btn-pill btn-sm" onClick={onClickSend}>Send Message</button>
       </div>
     </div>
-    )
+  )
 }
 
 export default MessageView
